d83cee7122139d62f23e5ef088a5b073afb975a3
MyAngelLYC <ycliu0930@gmail.com>
2016-02-06 15:59:56 +0800

    create

---
+++ server/server.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
+
+var server = http.createServer(function (request, response){
+	console.log(request.headers);
+	var filePath;
+	if(request.url == '/'){
+		filePath = 'client/index.html';
+	}else{
+		filePath = 'client' + request.url;
+	}
+	var absPath = '../' + filePath;
+	serveStaticFile(response, cache, absPath);
+});
+server.listen(3000);
+
+function serveStaticFile(response, cache, absPath){
+	if(cache[absPath]){
+		sendFile(response, absPath, cache[absPath]);
+	}else{
+		fs.exists(absPath, function(exists){
+			if(exists){
+				fs.readFile(absPath,function(err, data){
+					if(err){
+						send404File(response);
+					}else{
+						cache[absPath] = data;
+						sendFile(response, absPath, data);
+					};
+				});
+			}else{
+				send404File(response);
+			};
+		});
+	};
+}
+
+function sendFile(response, absPath, data){
+	response.writeHead(200, {'Content-Type': mime.lookup(path.basename(absPath))});
+	response.end(data);
+}
+
+function send404File(response){
+	console.error('404 resources not found!');
+	response.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
+	response.end('对不起，您访问的文件不存在');
+}
